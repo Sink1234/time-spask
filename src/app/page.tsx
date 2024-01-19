cf24e15abe2855050c6c271e1b7c93da2f022fd1
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 01:21:11 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import 'server-only'
 import Home from '@/components/Home-mobile/Home'
 import { Welcome, YhZav } from '@/interfaces'
 import fs from "fs"
-import { parseString } from "xml2js"
-import styles from './page.module.css'
+import { parseString } from 'xml2js'
 import { Montserrat } from "next/font/google"
 import Table from '@/components/ui-mob/Table'
 
@@ -15,8 +14,8 @@ const montserrat = Montserrat({
     variable: '--font-montserrat',
     subsets: ['latin'],
 })
-export const Services = (() => {
-  const xmldata = fs.readFileSync('public/rs202320.xml', 'utf-8')
+
+const xmldata = fs.readFileSync('public/rs202320.xml', 'utf-8')
  
       parseString(xmldata, function (err, results){ 
           if(err){
@@ -25,21 +24,10 @@ export const Services = (() => {
             let data = (JSON.stringify(results))
             fs.writeFileSync('public/data.json', data, 'utf-8')  
           }})
-        
-  const data: Welcome = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
-
-  return {
-      getData(): Welcome {
-          const data = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
-          return data
-      }
-      
-  }
-})()
 
 export default async function HomePage({id}:Iid) { 
-  const data = Services.getData()
-  const dataTable = Services.getData()
+  const data: Welcome = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
+  const dataTable = data
   let filtered
   id ? (
     filtered = data.YhZav.ListGroup[0].Group.filter(function (group){
