0eba33602724114295f7d0dfce2963b19ecf0a7c
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 00:18:09 +0300

    Update page.tsx

--- src/app/group/[name]/page.tsx
+++ src/app/group/[name]/page.tsx
@@ -1,11 +1,12 @@
 import HomePage from "@/app/page"
+import * as express from 'express';
 import { Request } from "express"
 declare namespace Express {
     export interface Request {
         id: string;
     }}
 
-  export default function groupPage (req:Request){
+  export default function groupPage (req:Request, res:Response){
       const id = decodeURIComponent(req.params.name)
       return(
           <HomePage id={id} />
