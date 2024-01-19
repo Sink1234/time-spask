522a371c5be663fe8fa7c9715c583ea195fcc719
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 00:23:22 +0300

    Update page.tsx

--- src/app/group/[name]/page.tsx
+++ src/app/group/[name]/page.tsx
@@ -3,7 +3,7 @@ import * as express from 'express';
 import { Request } from "express"
 declare namespace Express {
     export interface Request {
-        id: string;
+        name: string;
     }}
 
   export default function groupPage (req:Request, res:Response){
