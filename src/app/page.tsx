992b4cdc39c1d1fb94b14b12f381d8ead5ab6073
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 16:04:21 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,35 @@ import fs from "fs"
 import { parseString } from 'xml2js'
 import { Montserrat } from "next/font/google"
 import Table from '@/components/ui-mob/Table'
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const getFixturePath = (filename: string) => path.join(__dirname, '..', '..', 'public', filename);
 const montserrat = Montserrat({ 
     variable: '--font-montserrat',
     subsets: ['latin'],
 })
 
-export function first () {
-  const xmldata = fs.readFileSync('public/rs202320.xml', 'utf-8')
+
+export async function first () {
+  const xmldata = fs.readFileSync(getFixturePath('rs202320'), 'utf-8')
  
   parseString(xmldata, function (err, results){ 
       if(err){
         return null
       }else{
         let data = (JSON.stringify(results))
-        fs.writeFileSync('public/data.json', data, 'utf-8')  
+        fs.writeFileSync(getFixturePath(`data.json`), data, 'utf-8')  
   }})
 }
 
 export const Service=(()=>{
-  
   return{
     getData(){
-      const data: Welcome = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
+      const data: Welcome = JSON.parse(fs.readFileSync(getFixturePath(`data.json`), 'utf-8'))
       return data
     }
   }
