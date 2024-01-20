76ace1b92c3372c1c44a97e104343dd7f1fe4803
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 21:17:02 +0300

    Update page.tsx

--- src/app/group/[name]/page.tsx
+++ src/app/group/[name]/page.tsx
@@ -1,48 +1,41 @@
+import fs from "fs"
+import path from 'path';
+import {Montserrat} from "next/font/google"
+import {Suspense} from "react";
 import Table from "@/components/ui-mob/Table"
 import Home from "@/components/Home-mobile/Home"
-import { Montserrat } from "next/font/google"
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import fs from "fs"
-import { Welcome } from "@/interfaces";
-import { Suspense } from "react";
+import {Welcome} from "@/interfaces";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const getFixturePath = (filename: string) => path.join(process.cwd(), 'public', filename);
 
-const getFixturePath = (filename: string) => path.join(__dirname, '..', '..', '..', '..', 'public', filename);
+const montserrat = Montserrat({
+    variable: '--font-montserrat',
+    subsets: ['latin'],
+});
 
-const montserrat = Montserrat({ 
-  variable: '--font-montserrat',
-  subsets: ['latin'],
-})
+export default function groupPage(
+    req: { params: { name: string } }
+) {
 
-export default function groupPage (
-  req: { params: { name: string } }
-  ){
-    
-    const id = decodeURIComponent(req.params.name)
-    const data: Welcome = JSON.parse(fs.readFileSync(getFixturePath(`data.json`), 'utf-8'))
-    const dataTable: Welcome = JSON.parse(fs.readFileSync(getFixturePath(`data.json`), 'utf-8'))
-    let filtered
+    const id = decodeURIComponent(req.params.name);
+    const data: Welcome = JSON.parse(fs.readFileSync(getFixturePath(`data.json`), 'utf-8'));
+    const dataTable: Welcome = JSON.parse(fs.readFileSync(getFixturePath(`data.json`), 'utf-8'));
+    let filtered;
     id ? (
-      filtered = data.YhZav.ListGroup[0].Group.filter(function (group){
-      return group.$.Name === String(id)
-    })
-    ) : ('')
-  filtered ? data.YhZav.ListGroup[0].Group = filtered : ''
-    
-    
-    return(
-      <section >
-      <div className={montserrat.className}>
-        <Suspense >
-          <Table N={dataTable} />
-        </Suspense>
-        
-        <Home YhZav={data.YhZav} />
-      </div>
-    
-    </section>
+        filtered = data.YhZav.ListGroup[0].Group.filter(function (group) {
+            return group.$.Name === String(id)
+        })
+    ) : ('');
+    filtered ? data.YhZav.ListGroup[0].Group = filtered : '';
+
+    return (
+        <section>
+            <div className={montserrat.className}>
+                <Suspense>
+                    <Table N={dataTable}/>
+                </Suspense>
+                <Home YhZav={data.YhZav}/>
+            </div>
+        </section>
     )
 }
