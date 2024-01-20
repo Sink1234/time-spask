d8aced57c64878d690ae6827d5a05d5c848d674a
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 11:27:29 +0300

    Update page.tsx

--- src/app/group/[name]/page.tsx
+++ src/app/group/[name]/page.tsx
@@ -1,6 +1,4 @@
-import HomePage, { Service} from "@/app/page"
-import fs from "fs"
-import { Welcome } from '@/interfaces'
+import { Service} from "@/app/page"
 import Table from "@/components/ui-mob/Table"
 import Home from "@/components/Home-mobile/Home"
 import { Montserrat } from "next/font/google"
@@ -13,14 +11,13 @@ const montserrat = Montserrat({
 
 
 
-
-const dataTable: Welcome = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
 export default function groupPage (
   req: { params: { name: string } }
   ){
     const id = decodeURIComponent(req.params.name)
     const d = Service()
     const data = d.getData()
+    const dataTable = d.getData()
     let filtered
     id ? (
       filtered = data.YhZav.ListGroup[0].Group.filter(function (group){
