0f5663b067bfce5827671717bdef010d049864f3
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-20 11:38:37 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,20 @@ const montserrat = Montserrat({
     subsets: ['latin'],
 })
 
-const xmldata = fs.readFileSync('public/rs202320.xml', 'utf-8')
+export function first () {
+  const xmldata = fs.readFileSync('public/rs202320.xml', 'utf-8')
  
-parseString(xmldata, function (err, results){ 
-    if(err){
-      return null
-    }else{
-      let data = (JSON.stringify(results))
-      fs.writeFileSync('public/data.json', data, 'utf-8')  
-}})
+  parseString(xmldata, function (err, results){ 
+      if(err){
+        return null
+      }else{
+        let data = (JSON.stringify(results))
+        fs.writeFileSync('public/data.json', data, 'utf-8')  
+  }})
+}
 
 export const Service=(()=>{
+  
   return{
     getData(){
       const data: Welcome = JSON.parse(fs.readFileSync('public/data.json', 'utf-8'))
@@ -31,7 +34,6 @@ export const Service=(()=>{
   }
 })
           
-
 export default async function HomePage() { 
   const data = Service()
   
