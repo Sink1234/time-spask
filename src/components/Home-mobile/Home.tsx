cc8f9edaef760aabb00f82be40bb0532b797a77d
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-01-21 13:47:58 +0300

    Update Home.tsx

--- src/components/Home-mobile/Home.tsx
+++ src/components/Home-mobile/Home.tsx
@@ -19,7 +19,7 @@ export default async function Home(YhZav: Welcome) {
         (
                 <PageWrapper >
                     <div className={styles.wrapper}>
-                        <h3>Кабинеты обновляются КАЖДЫЙ день, будте внимательны!</h3>
+                        <h3>Кабинеты обновляются КАЖДЫЙ день, будьте внимательны!</h3>
                         {YhZav.YhZav.ListGroup[0].Group.map((group) => (
                                 <Group key={group.$.Name} group={group} dataMon={dataMon} />
                             ))}
