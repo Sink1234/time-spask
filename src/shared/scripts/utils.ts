62f064e7f3a9ca821ff2d52c0213c63ff58c921b
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-02-11 12:02:28 +0300

    Delete src directory

--- src/shared/scripts/utils.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs/promises";
-import {join} from "path";
-
-const rootPath = process.cwd()
-
-export async function readDataJsonFile(name: string) {
-    const data = await fs.readFile(join(rootPath, "src","shared", "data", name), {encoding: "utf8"})
-    return JSON.parse(data)
-}
