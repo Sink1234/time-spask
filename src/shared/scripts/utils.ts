be7c49a2baff47d92b93a2453597f60c24ae072c
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2025-09-10 13:00:40 +0300

    Update utils.ts

--- src/shared/scripts/utils.ts
+++ src/shared/scripts/utils.ts
@@ -1,9 +1,15 @@
 import fs from "fs/promises";
-import {join} from "path";
+import { resolve } from "path";
 
-const rootPath = process.cwd()
+const rootPath = process.cwd();
 
 export async function readDataJsonFile(name: string) {
-    const data = await fs.readFile(join(rootPath, "src","shared", "lib", "data", name), {encoding: "utf8"})
-    return JSON.parse(data)
+    const filePath = resolve(rootPath, "src", "shared", "lib", "data", name);
+    const data = await fs.readFile(filePath, { encoding: "utf8" });
+    const cleanedData = data.replace(/^\uFEFF/, ''); // Удаляем BOM
+    try {
+        return JSON.parse(cleanedData);
+    } catch (err) {
+        throw new Error(`Невалидный JSON в файле ${name}: ${err.message}`);
+    }
 }
