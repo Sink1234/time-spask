3cc94490cd9666d76efc0c893fb73525cc53cf6c
ssppaasskk <ssppaasskk@gmail.com>
2026-09-02 10:19:47 +0300

    Update Header.tsx

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -15,7 +15,7 @@ const navList = [
     {link: "/room/onTeacher", content: "Кабинеты по преподавателям"},
     {link: '/main/group', content: "Расписание на семестр для групп"},
     {link: '/main/teacher', content: "Расписание для преподавателей"},
-     { id: 5, title: 'Скачать замены', link: '/zamena.docx', download: true },
+     { id: 5, title: 'Скачать замены', link: '/public/zamena.docx', download: true },
 ];
 const Navbar = () => {
     const [open, setOpen] = useState(false);
